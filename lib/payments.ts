6a72a7d3f55a0f81d3af347a2e64f65258930753
Forest-Coder <80033503+Forest-Coder@users.noreply.github.com>
2025-01-13 10:03:45 +0900

    widget

---
+++ lib/payments.ts
@@ -0,0 +1,3 @@
+export const monthlyPlanId: string = "price_1QfE9bCOaBMplNm7g0gDHlkL";
+export const yearlyPlanId: string = "price_1QfE9bCOaBMplNm70FXyj8e7";
+export const maxFreeProjects: number = 3;
