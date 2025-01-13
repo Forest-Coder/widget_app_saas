8af772725382d312afecf70ab5a883684fe0e993
Forest-Coder <80033503+Forest-Coder@users.noreply.github.com>
2025-01-13 16:21:28 +0900

    Added Put cors only to feedback that works

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
+import { NextResponse } from "next/server";
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/projects(.*)", "/payments(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
@@ -9,22 +9,3 @@ export default clerkMiddleware((auth, req) => {
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-
-
-import { NextResponse } from "next/server";
-
-export function middleware() {
-  // retrieve the current response
-  const res = NextResponse.next()
-
-  // add the CORS headers to the response
-  res.headers.append('Access-Control-Allow-Credentials', "true")
-  res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
-  res.headers.append('Access-Control-Allow-Methods', 'POST')
-  res.headers.append(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-
-  return res
-}
