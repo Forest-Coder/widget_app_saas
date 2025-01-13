7aca8942bd1515249a4d9cd37c794be9390f4fc2
Forest-Coder <80033503+Forest-Coder@users.noreply.github.com>
2025-01-13 16:36:03 +0900

    Directly extract fields

--- app/api/add-feedback/route.ts
+++ app/api/add-feedback/route.ts
@@ -5,34 +5,36 @@ import { feedbacks } from "@/db/schema";
 
 export async function POST(req: Request) {
     //Validation
-    const { data } = await req.json()
-    if (data.p_project_id == null) {
-        return new Response(JSON.stringify({data: { error: "Bad Request" }}), { status: 400 });
+    const response = await req.json()
+    const { p_project_id, p_user_name, p_user_email, p_message, p_rating } = response
+    console.log(response)
+    if (p_project_id == null) {
+        return new Response(JSON.stringify({ data: { error: "Bad Request" } }), { status: 400 });
     }
-    if (data.p_user_name == null) {
-        return new Response(JSON.stringify({data:{ error: "No name given" }}), { status: 400 });
+    if (p_user_name == null) {
+        return new Response(JSON.stringify({ data: { error: "No name given" } }), { status: 400 });
     }
-    if (data.p_user_email == null) {
-        return new Response(JSON.stringify( {data:{ error: "No email given" }}), { status: 400 });
+    if (p_user_email == null) {
+        return new Response(JSON.stringify({ data: { error: "No email given" } }), { status: 400 });
     }
-    if (data.p_message == null) {
-        return new Response(JSON.stringify({data:{ error: "No message given" }}), { status: 400 });
+    if (p_message == null) {
+        return new Response(JSON.stringify({ data: { error: "No message given" } }), { status: 400 });
     }
-    if (data.p_rating == null) {
-        return new Response(JSON.stringify({data:{ error: "No rating given" }}), { status: 400 });
+    if (p_rating == null) {
+        return new Response(JSON.stringify({ data: { error: "No rating given" } }), { status: 400 });
     }
 
     try {
         await db.insert(feedbacks).values({
-            projectId: data.p_project_id,
-            userName: data.p_user_name,
-            userEmail: data.p_user_email,
-            message: data.p_message,
-            rating: data.p_rating
+            projectId: p_project_id,
+            userName: p_user_name,
+            userEmail: p_user_email,
+            message: p_message,
+            rating: p_rating
         })
-        return new Response(JSON.stringify({data: { returnedData: "Feedback successfully added" }}), { status: 200 });
+        return new Response(JSON.stringify({ data: { returnedData: "Feedback successfully added" } }), { status: 200 });
     } catch (error) {
         console.error(error)
     }
-    return new Response(JSON.stringify({data: { error: "Server-side error" }}), { status: 500 });
+    return new Response(JSON.stringify({ data: { error: "Server-side error" } }), { status: 500 });
 }
