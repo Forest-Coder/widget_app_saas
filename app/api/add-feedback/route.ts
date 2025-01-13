b21eea351a44e0a337f139a3b79356abd48929f5
Forest-Coder <80033503+Forest-Coder@users.noreply.github.com>
2025-01-13 15:38:36 +0900

    changed data format

--- app/api/add-feedback/route.ts
+++ app/api/add-feedback/route.ts
@@ -7,19 +7,19 @@ export async function POST(req: Request) {
     //Validation
     const { data } = await req.json()
     if (data.p_project_id == null) {
-        return new Response(JSON.stringify({ error: "Bad Request" }), { status: 400 });
+        return new Response(JSON.stringify({data: { error: "Bad Request" }}), { status: 400 });
     }
     if (data.p_user_name == null) {
-        return new Response(JSON.stringify({ error: "No name given" }), { status: 400 });
+        return new Response(JSON.stringify({data:{ error: "No name given" }}), { status: 400 });
     }
     if (data.p_user_email == null) {
-        return new Response(JSON.stringify({ error: "No email given" }), { status: 400 });
+        return new Response(JSON.stringify( {data:{ error: "No email given" }}), { status: 400 });
     }
     if (data.p_message == null) {
-        return new Response(JSON.stringify({ error: "No message given" }), { status: 400 });
+        return new Response(JSON.stringify({data:{ error: "No message given" }}), { status: 400 });
     }
     if (data.p_rating == null) {
-        return new Response(JSON.stringify({ error: "No rating given" }), { status: 400 });
+        return new Response(JSON.stringify({data:{ error: "No rating given" }}), { status: 400 });
     }
 
     try {
@@ -30,9 +30,9 @@ export async function POST(req: Request) {
             message: data.p_message,
             rating: data.p_rating
         })
-        return new Response(JSON.stringify({ message: "Feedback successfully added" }), { status: 200 });
+        return new Response(JSON.stringify({data: { returnedData: "Feedback successfully added" }}), { status: 200 });
     } catch (error) {
         console.error(error)
     }
-    return new Response(JSON.stringify({ error: "Server-side error" }), { status: 500 });
+    return new Response(JSON.stringify({data: { error: "Server-side error" }}), { status: 500 });
 }
