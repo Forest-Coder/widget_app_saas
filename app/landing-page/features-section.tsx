072a73e1cf2ab038fdfbfe49768f5d263167f999
Forest-Coder <80033503+Forest-Coder@users.noreply.github.com>
2025-01-17 15:15:16 +0900

    Increase font size

--- app/landing-page/features-section.tsx
+++ app/landing-page/features-section.tsx
@@ -31,7 +31,7 @@ const features = [
 const FeaturesSection = () => {
   return (
     <section className=" container mx-auto max-w-screen-xl px-4 my-24 flex items-center flex-col">
-      <h2 className="mb-6 text-2xl font-bold">Features</h2>
+      <h2 className="mb-6 text-3xl font-bold">Features</h2>
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {features.map((feature, index) => (
